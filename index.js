9e327345bce583adcfb0a89b66f022380fae0126
John Berean <jmberean@gmail.com>
2018-10-22 11:20:03 -0400

    vue

--- index.js
+++
@@ -1,7 +0,0 @@
-// ctrl + space : gives all possible js command promts
-const math = require('./math');
-
-var something = math.add(2,2);
-
-console.log("hello world");
-console.log("2 + 2 ndo= " + something);
--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-// ctrl + space : gives all possible js command promts
+const express = require('express');
+const game = require('./game/controller');
+
+const app = express();
+
+const port = 3000;
+const server = "localhost";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/", express.static(__dirname + "/../dist/"));
+app.use('/game', game);
+
+app.listen(port);
+
+// eslint-disable-next-line no-console
+console.log(`listening on: http://${server}:${port}`);
