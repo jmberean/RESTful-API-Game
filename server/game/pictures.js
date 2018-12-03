d04d0ba5d38fddc6157de63e47b019d244077889
john beren <jmberean@gmail.com>
2018-12-03 10:59:52 -0500

    working files

--- server/game/pictures.js
+++
@@ -1,5 +0,0 @@
-var axios = require("axios");
-
-module.exports = () =>
-    axios.get('https://api.imgflip.com/get_memes')
-    .then( response => response.data.data.memes );
--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h2>My name is Moshe Plotkin</h2>
-  </div>
-</template>
